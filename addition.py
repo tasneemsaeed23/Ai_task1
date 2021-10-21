88923f0bd387d17ace70100c24f49aef5b272ddb
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2021-10-22 01:08:51 +0200

    solved 1Q,2Q & 3Q

---
+++ addition.py
@@ -0,0 +1,15 @@
+
+
+"""
+Run python autograder.py
+"""
+
+
+def add(a, b):
+    "Return the sum of a and b"
+    print("Passed a = %s and b = %s, returning a + b = %s" % (a, b, a + b))
+    return a + b
+
+    
+
+add(5,3)
